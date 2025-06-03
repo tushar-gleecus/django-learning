fecc9893528fba36e7b97539a510098e1784587d
tushar-gleecus <tushar.canchi@gleecus.com>
2025-06-03 13:46:51 +0530

    Added forms and veiw entered form data in admin

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from django.db import models
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} - Feedback"
 # Create your models here.
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
 
-def index(request):
-    context = {
-        "user_name": "Tushar",
-        "topics": ["HTML", "CSS", "Python", "Django"]
-    }
-    return render(request, "polls/index.html", context)
+def feedback_view(request):
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "polls/result.html", {"name": form.cleaned_data["name"]})
+    else:
+        form = FeedbackForm()
+    return render(request, "polls/feedback.html", {"form": form})
